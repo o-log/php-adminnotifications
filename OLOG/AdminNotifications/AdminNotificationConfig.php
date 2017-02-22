916f0d4b591e715002cdd3a6a708998c02fc900a
D.Tsaritov <tsaritov@gmail.com>
2017-02-22 14:47:09 +0300

    1

--- OLOG/AdminNotifications/AdminNotificationConfig.php
+++ OLOG/AdminNotifications/AdminNotificationConfig.php
@@ -4,8 +4,27 @@ namespace OLOG\AdminNotifications;
 
 class AdminNotificationConfig
 {
-    const ADMIN_NOTIFICATIONS_DB_NAME=' db_admin_notifications';
+    const ADMIN_NOTIFICATIONS_DB_NAME='db_admin_notifications';
+    const ADMIN_NOTIFICATIONS_KEYVALUE_KEY_EMAIL_LIST='ADMIN_NOTIFICATIONS_EMAIL_LIST';
+
     static protected $admin_actions_base_classname;
+    static protected $email_from;
+
+    /**
+     * @return mixed
+     */
+    public static function getEmailFrom()
+    {
+        return self::$email_from;
+    }
+
+    /**
+     * @param mixed $email_from
+     */
+    public static function setEmailFrom($email_from)
+    {
+        self::$email_from = $email_from;
+    }
 
     /**
      * @return mixed
