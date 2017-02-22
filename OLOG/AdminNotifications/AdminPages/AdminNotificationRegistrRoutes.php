9c4f94a308e2c9deb9fb8e6be29f16fbd686a20f
D.Tsaritov <tsaritov@gmail.com>
2017-02-22 15:38:26 +0300

    1

---
+++ OLOG/AdminNotifications/AdminPages/AdminNotificationRegistrRoutes.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 22.02.17
+ * Time: 15:36
+ */
+
+namespace OLOG\AdminNotifications\AdminPages;
+
+
+class AdminNotificationRegistrRoutes
+{
+    static public function registr(){
+        \OLOG\Router::processAction(AdminNotificationsListAction::class, 0);
+    }
+}
