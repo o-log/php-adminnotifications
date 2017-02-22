329fe0b98c75ac8a090298a5aacbbbd781403a37
D.Tsaritov <tsaritov@gmail.com>
2017-02-22 15:09:10 +0300

    1

--- OLOG/AdminNotifications/AdminPages/AdminNotificationsAdminMenu.php
+++ OLOG/AdminNotifications/AdminPages/AdminNotificationsAdminMenu.php
@@ -13,7 +13,7 @@ class AdminNotificationsAdminMenu implements InterfaceMenu
         $menu_arr = [];
 
         if (Auth::currentUserHasAnyOfPermissions([Permissions::PERMISSION_ADMINNOTIFICATIONS_MANAGE])) {
-            $menu_arr[] = new MenuItem('Авторизация соц. сети', '', [
+            $menu_arr[] = new MenuItem('Уведомления', '', [
                 new MenuItem('O_o', '', [], 'glyphicon glyphicon-user')
             ], 'glyphicon glyphicon-log-in');
         }
